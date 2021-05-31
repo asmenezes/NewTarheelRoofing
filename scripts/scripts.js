d8a8e8eb6fc64aab7accf5f679382c44aad7d2c7
asmenezes <asmenezes23@gmail.com>
2021-05-31 15:41:03 -0400

    Menu Scroll Code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,3 +18,76 @@ win.onscroll = function(){
    var sTop = (this.pageYOffset || docEl.scrollTop)  - (docEl.clientTop || 0);
    navbar.style.backgroundColor =  sTop > 100 ? "var(--accent-color)":"transparent" ;
 };
+/*
+//on document ready
+//get position of all the "links"
+//if scroll position is "under" a certain link take all highlights away then give its menu item a highlight
+//on document resize re check position of the "links"
+#about
+#expectations
+#services
+#contact
+#careers
+
+*/
+let linksArr = [];
+let posArr = [];
+let sectionArr = [];
+let currentIndex = 0;
+document.onload = getLinks();
+document.onload = getPositions();
+window.onresize = getPositions();
+
+function getPositions(){
+for (var i = 0; i < sectionArr.length; i++) {
+  posArr[i] = sectionArr[i].getBoundingClientRect();
+  console.log(posArr[i]);
+}
+}
+function doSomething(pos){
+  let cont = true;
+
+  for (var i = 0; i < posArr.length; i++) {
+      linksArr[i].classList.remove("high");
+
+  }
+for (var i = 0; i < posArr.length && cont; i++) {
+  if(posArr[i].top <= pos){
+    cont = false;
+    linksArr[i].classList.add("high");
+    console.log(linksArr[i].classList)
+  }
+}
+}
+function getLinks(){
+linksArr[0] = document.getElementById('navcareers');
+linksArr[1] = document.getElementById('navcontact');
+linksArr[2] = document.getElementById('navservices');
+linksArr[3] = document.getElementById('navexpectations');
+linksArr[4] = document.getElementById('navabout');
+
+
+sectionArr[0] = document.getElementById('careers');
+sectionArr[1] = document.getElementById('contact');
+sectionArr[2] = document.getElementById('services');
+sectionArr[3] = document.getElementById('expectations');
+sectionArr[4] = document.getElementById('about');
+console.log(linksArr)
+console.log(sectionArr)
+
+}
+
+
+
+document.addEventListener('scroll', function(e) {
+  lastKnownScrollPosition = window.scrollY;
+
+
+  setTimeout(function() {
+      doSomething(lastKnownScrollPosition);
+
+
+
+
+  },500);
+});
